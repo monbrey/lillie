75b97297156e142507a19b3cdab0ec8ae964f6b7
Ryan Munro <monbrey@gmail.com>
2024-10-25 07:14:10 +1100

    fix: capture custom emoji id only

--- src/bot/commands/chat-input/config.ts
+++ src/bot/commands/chat-input/config.ts
@@ -3,6 +3,30 @@ import { ApplicationCommandOptionType, ApplicationIntegrationType, ChannelType, 
 import { isApplicationCommandGuildInteraction } from "discord-api-types/utils/v10";
 import { updateConfigForGuild, type Config } from "../../../database/models/config.js";
 
+const parseEmoji = (text: string) => {
+	let val = text;
+	if (val.includes("%")) {
+		val = decodeURIComponent(text);
+	}
+
+	if (!val.includes(":")) {
+		return {
+			animated: false,
+			name: text,
+			id: undefined,
+		};
+	}
+
+	const match = /<?(?:(?<animated>a):)?(?<name>\w{2,32}):(?<id>\d{17,19})?>?/.exec(val);
+	console.log(match);
+
+	return match && {
+		animated: Boolean(match[1]),
+		name: match[2],
+		id: match[3],
+	};
+};
+
 export const data: RESTPostAPIChatInputApplicationCommandsJSONBody = {
 	name: "config",
 	description: "Modify lilyboard configuration options",
@@ -96,7 +120,8 @@ export const execute = async (api: API, interaction: APIChatInputApplicationComm
 		if (option.name === "channel" && option.type === ApplicationCommandOptionType.Channel) {
 			config[subcommand.name === "standard" ? "standard_channel_id" : "gold_channel_id"] = option.value;
 		} else if (option.name === "emoji" && option.type === ApplicationCommandOptionType.String) {
-			config[subcommand.name === "standard" ? "standard_emoji" : "gold_emoji"] = option.value;
+			const emoji = parseEmoji(option.value);
+			config[subcommand.name === "standard" ? "standard_emoji" : "gold_emoji"] = emoji?.id;
 		} else if (option.name === "threshold" && option.type === ApplicationCommandOptionType.Integer) {
 			config[subcommand.name === "standard" ? "standard_threshold" : "gold_threshold"] = option.value;
 		}
